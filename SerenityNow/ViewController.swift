d235c9bf7a97abaef073ccc0c976e07b72f10d18
John Bruce <jbruce2112@gmail.com>
2017-05-28 10:13:20 -0400

    Initial Commit

---
+++ SerenityNow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerenityNow
+//
+//  Created by John on 5/28/17.
+//  Copyright © 2017 Bruce32. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
